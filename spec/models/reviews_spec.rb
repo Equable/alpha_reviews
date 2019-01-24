295fcecdd7489988f7320a579fb69a0e224817ab
Equable <escnoble@gmail.com>
2019-01-24 14:52:00 -0500

    almost done

--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-    context "Reviews table should exist" do
-        let!(:review1) { FactoryBot.create(:review) }
-        let!(:review2) { FactoryBot.build(:review) }
-        it "should be able to save a new instance of review" do
-            expect(review1.save).to eq(true)
-            expect(Review.first).to eq(review1)
-        end
-        it "should fail if there is no user" do
-            review2.user = nil
-            expect(review2.save).to eq(false)
-            binding.pry
-        end
-    end
+    it { should validate_presence_of(:rating) }
+    it { should validate_inclusion_of(:rating).in(1..100)}
 end
