41c56f69f780d247449d37f514fcc86992ae8687
Tony Phu <tonyphu@Tonys-MacBook-Pro.local>
2019-01-29 11:07:55 -0500

    getting jpeg to upload avatar picture locally

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # attr_accessor :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
+
   def username
     return self.email.split('@')[0]
   end
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 end
