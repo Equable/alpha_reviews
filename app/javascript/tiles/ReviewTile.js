100e5d24ffc0cb2f610649bdb0dd230b2032f3d9
Equable <escnoble@gmail.com>
2019-01-29 17:13:49 -0500

    added edit feature for reviews, need to add delete

--- app/javascript/tiles/ReviewTile.js
+++ app/javascript/tiles/ReviewTile.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router'
 
 const ReviewTile = props => {
-    return (
-        <div className='review-tile'>
-            <h4>Rating: {props.review.rating}/100</h4>
-            <h4>User: {props.review.user_id}</h4>
-            <h6>Comment:</h6>
-            <p>{props.review.comment}</p>
-            <Link to={`/reviews/${props.review.id}/edit`}>Edit</Link>
+  return (
+    <div className='review-tile'>
+      < h4 > Rating: {props.review.rating}/5</h4 > 
+      <h4>User: {props.review.user_id}</h4> 
+      <h6>Comment:</h6> 
+      <p>{props.review.comment}</p> 
+      <div className="row">
+        <div className="small-6 column ">
+          <button onClick={props.onClick}>Edit</button>
+          <button>Delete</button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ReviewTile;
